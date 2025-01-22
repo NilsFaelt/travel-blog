0567142ac610c0ec685569bbb4357cfe4fdcf5ca
Nils Falt <nils.faelt@gmail.com>
2025-01-22 09:32:00 +0100

    work brancg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { MainLayout } from "@/layout";
 export default function Home() {
   return (
     <MainLayout>
-      <h1>IM MAIN BRANCH</h1>
+      <h1>IM WORK BRANCH</h1>
       <HomeView />
     </MainLayout>
   );
